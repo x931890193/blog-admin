17dd54d2afbc958448b332c1526f85d485988409
sato <ysudqfs@163.com>
2022-08-11 14:21:12 +0800

    add more router

--- src/views/system/setting/index.vue
+++ src/views/system/setting/index.vue
@@ -4,9 +4,9 @@
       <el-tab-pane label="关于我" name="first">
         <AboutSetting/>
       </el-tab-pane>
-      <el-tab-pane label="菜单设置" name="second">
-        <FrontMenuSetting/>
-      </el-tab-pane>
+<!--      <el-tab-pane  label="菜单设置" name="second">-->
+<!--        <FrontMenuSetting/>-->
+<!--      </el-tab-pane>-->
       <el-tab-pane label="前台网站设置" name="third">
         <SiteSetting/>
       </el-tab-pane>
