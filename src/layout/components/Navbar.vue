a8a188a328b136a669fca6559c6735d002c0779b
sato <ysudqfs@163.com>
2022-08-15 23:34:30 +0800

    fix

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -25,7 +25,7 @@
 
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="require(`@/assets/avatar/${this.$store.state.user.avatar}`)" class="user-avatar">
+          <img :src="(`${this.$store.state.user.avatar}`)" class="user-avatar">
           <i class="el-icon-caret-bottom"/>
         </div>
         <el-dropdown-menu slot="dropdown">
